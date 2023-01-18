aa1b2cfa660ffdddfba7e4702ec296bb0494440b
OrlandoNeto024 <orlandoparacamposn@gmail.com>
2023-01-18 18:30:34 -0300

    reset modificacao

--- 01-2023/0 - Exercicios/listaDeTarefas/script.js
+++ 01-2023/0 - Exercicios/listaDeTarefas/script.js
@@ -1,17 +1,16 @@
 const inputElement = document.getElementById('input-nova-tarefa');
 var caixas = 0;
-while(caixas <= 6){
-    inputElement.addEventListener('keyup', function(e){
-        var key = e.keyCode;
-        if(key == 13){
-    
-            var tarefaInsertion = `<div id="main"><input type="checkbox" id="tarefa" name="tarefa"><label for="tarefa">${document.getElementById('input-nova-tarefa').value}</label><div id="delete"><a href="#">X</a></div></div>`;
-    
-            document.getElementById('container').innerHTML += tarefaInsertion;
-            caixas += 1
-        }
-    })
-}
+
+inputElement.addEventListener('keyup', function(e){
+    var key = e.keyCode;
+    if(key == 13){
+
+        var tarefaInsertion = `<div id="main"><input type="checkbox" id="tarefa" name="tarefa"><label for="tarefa">${document.getElementById('input-nova-tarefa').value}</label><div id="delete"><a href="#">X</a></div></div>`;
+
+        document.getElementById('container').innerHTML += tarefaInsertion;
+    }
+})
+
 
 
 /*<input type="checkbox" id="tarefa" name="tarefa">
