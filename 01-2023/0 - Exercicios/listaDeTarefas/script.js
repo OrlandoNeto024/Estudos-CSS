6ccec2851b2f823751d843db0744c53a82471564
OrlandoNeto024 <orlandoparacamposn@gmail.com>
2023-01-20 07:27:16 -0300

    Tarefas ajustadas

--- 01-2023/0 - Exercicios/listaDeTarefas/script.js
+++ 01-2023/0 - Exercicios/listaDeTarefas/script.js
@@ -1,23 +1,33 @@
 const inputElement = document.getElementById('input-nova-tarefa');
-var caixas = 0;
+
 
 inputElement.addEventListener('keyup', function(e){
     var key = e.keyCode;
     if(key == 13){
+        
+        var slugInput = document.getElementById('input-nova-tarefa').value;
+        slugInput = slugInput.replaceAll(' ', '-')
 
-        var tarefaInsertion = `<div id="main"><input type="checkbox" id="tarefa" name="tarefa"><label for="tarefa">${document.getElementById('input-nova-tarefa').value}</label><div id="delete"><a href="#">X</a></div></div>`;
+        var tarefaInsertion = `<div id="main"><input onclick="check()" type="checkbox" id="${slugInput}" name="${slugInput}"><label for="${slugInput}">${document.getElementById('input-nova-tarefa').value}</label><div id="delete"><a href="#">X</a></div></div>`;
 
-        document.getElementById('container').innerHTML += tarefaInsertion;
+        
+        document.getElementById('main-area').innerHTML += tarefaInsertion;
+        document.getElementById('input-nova-tarefa').value = ''
+        
     }
 })
 
+/*function check(){
+    var checkBox = document.getElementById(`${slugInput}`);
+    
+    
+    if(checkBox.checked == true){
+        alert(`${checkBox.label.value}`)
+    }
+    else{
+        if(checkBox.checked == false){
+            label.style.textDecoration = 'none'
+        }
+    }
+}*/
 
-
-/*<input type="checkbox" id="tarefa" name="tarefa">
-<label for="tarefa">Tarefa</label>
-
-<div id="delete">
-    <a href="#">X</a>
-</div>*/
-
-/* PARA AMANHÃ: Como adicionar mais elementos com valores diferentes */
